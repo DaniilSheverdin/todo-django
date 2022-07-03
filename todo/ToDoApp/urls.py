77153f07d3720f0a89668a74a91267901fdb17fb
Daniil Sheverdin <sheverdin.daniil@gmail.com>
2022-07-03 16:16:32 +0300

    CRUD completed

--- todo/ToDoApp/urls.py
+++ todo/ToDoApp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import TaskList
+from .views import TaskCreate, TaskDelete, TaskList, TaskDetail, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('add-task/', TaskCreate.as_view(), name='add-task'),
+    path('delete-task/<int:pk>', TaskDelete.as_view(), name='delete-task'),
+    path('update-task/<int:pk>', TaskUpdate.as_view(), name='update-task'),
 ]
--- todo/ToDoApp/views.py
+++ todo/ToDoApp/views.py
@@ -2,11 +2,45 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Task
 # Create your views here.
 
 
 class TaskList(ListView):
-    """controller for task list"""
+    """controller for show task list"""
     model = Task
-   # template = loader.get_template('ToDoApp/task_list.html')
+    context_object_name = "tasks"
+    template_name = 'ToDoApp/task.list.html'
+
+
+class TaskDetail(DetailView):
+    """controller for show single task"""
+    model = Task
+    context_object_name = "task"
+    template_name = 'ToDoApp/task.html'
+
+
+class TaskCreate(CreateView):
+    """controller for create task"""
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy('tasks')
+    template_name = 'ToDoApp/add.task.form.html'
+
+
+class TaskUpdate(UpdateView):
+    """controller for update task"""
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy('tasks')
+    template_name = 'ToDoApp/add.task.form.html'
+
+
+class TaskDelete(DeleteView):
+    """controller for delete task"""
+    model = Task
+    success_url = reverse_lazy('tasks')
+    template_name = 'ToDoApp/task.delete.confirm.html'
